360d29861d5859cfffdd621e9a3cc33a63ea1ea2
Melvin Reji John <melvin.john@bbc.co.uk>
2019-10-05 19:29:56 +0100

    Added Public fetch APIs

--- Tests/MyNetworkingPackageTests/MyNetworkingPackageTests.swift
+++ Tests/MyNetworkingPackageTests/MyNetworkingPackageTests.swift
@@ -2,14 +2,4 @@ import XCTest
 @testable import MyNetworkingPackage
 
 final class MyNetworkingPackageTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(MyNetworkingPackage().text, "Hello, World!")
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
